80ab9a866742529f0752c2446b0ec861a2fa0bd1
Dina <dmrabanal@gmail.com>
2018-11-21 19:10:48 -0700

    Initial commit

---
+++ src/classes/Dina.java
@@ -0,0 +1,17 @@
+package classes;
+//object and class can be used interchangably
+public class Dina {//public is acess modifier class declarying class object dina is name of the class
+
+  public static String getMyName(){ //String is a class in this context also the return type of this method
+  String name = "Dina";
+  return name;
+  }
+  public String getMyName1(){ //String is a class in this context also the return type of this method
+    String name = "Dina";
+    return name;
+  }
+  public String getYourName(String string){
+    return string;
+  }
+
+}
---
+++ src/classes/Main.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Dina dina = new Dina();
+    System.out.println(dina.getYourName("JR"));
+
+  }
+
+}
